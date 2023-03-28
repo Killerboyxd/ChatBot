0cd0fb0a2137625a67a84d7af649b6e8e56a34cc
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-03-28 10:03:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,9 +47,7 @@ async def start(client, message):
         await message.reply("ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ ɪɴ ᴘᴇʀsᴏɴᴀʟ",
                             reply_markup=buttons)
         
-    else:
-        ]
-        ]
+    else= 
         video = "https://telegra.ph/file/9584b19633bf5b31faa12.jpg"
         await message.reply_video(video, caption=f"""ʜᴇʟʟᴏ[{message.from_user.first_name}](tg://user?id={message.from_user.id}),
 ɪ ᴀᴍ ᴄʜᴀᴛʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs ...
